1e48974cb991d455d7b4b8a53482c085e7b85e03
nsok1007 <nsok1007@gmail.com>
2026-03-14 13:08:50 -0500

    Assignment Finished

--- unicode.js
+++ unicode.js
@@ -86,15 +86,15 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0);
+let thirdCodePoint = inputString1.charCodeAt(2); 
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72,101,108,108);
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let swappedString = String.fromCharCode(104,97,117,110,99,76);
 
 // Log all results
 console.log({
